b012c61718ecc36e7cf66fa5a870c07e66ec3d6a
Keshav Agarwal <keshav3064@gmail.com>
2025-10-02 06:59:46 +0530

    First commit

---
+++ firebase/client.ts
@@ -0,0 +1,18 @@
+import { initializeApp,getApps,getApp} from "firebase/app";
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCR15_uOGwSX4BWIt4kfLZVArtyxuy4trY",
+    authDomain: "prepwise-8de22.firebaseapp.com",
+    projectId: "prepwise-8de22",
+    storageBucket: "prepwise-8de22.firebasestorage.app",
+    messagingSenderId: "771571550975",
+    appId: "1:771571550975:web:3600f67967797a16a75aca",
+    measurementId: "G-0GQBWDKBW1"
+};
+
+const app =!getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
