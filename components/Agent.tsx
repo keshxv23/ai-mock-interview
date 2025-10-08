7662261f0ddf5fb037c744b8a67b0225c7ff2a8c
Keshav Agarwal <keshav3064@gmail.com>
2025-10-08 16:00:41 +0530

    implement interview session

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -19,7 +19,7 @@ interface SavedMessage{
     content:string;
 }
 
-const Agent = ({ userName,userId,type,questions }: AgentProps) => {
+const Agent = ({ userName,userId,type,interviewId,questions }: AgentProps) => {
     const router = useRouter();
     const [isSpeaking, setIsSpeaking] = useState(false);
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
@@ -59,8 +59,30 @@ const Agent = ({ userName,userId,type,questions }: AgentProps) => {
        }
     },[])
 
+    const handleGenerateFeedback = async (messages:
+    SavedMessage[]) => {
+        console.log('Generate feedback here');
+
+        const { success, id } = {
+            success:true,
+            id: 'feedback-id'
+        }
+        if(success && id){
+            router.push(`/interview/${interviewId}/feedback`);
+        }else{
+            console.log('Error saving feedback');
+            router.push('/');
+        }
+    }
+
     useEffect(() => {
-        if (callStatus === CallStatus.FINISHED) router.push('/');
+        if(callStatus === CallStatus.FINISHED){
+            if(type === 'generate'){
+                router.push('/');
+            }else{
+                handleGenerateFeedback(messages);
+            }
+        }
     }, [messages, callStatus,type,userId]);
 
     const handleCall = async () => {
@@ -77,8 +99,7 @@ const Agent = ({ userName,userId,type,questions }: AgentProps) => {
                         username: userName,
                         userid: userId,
                     },
-                }
-            );
+                });
         } else {
             let formattedQuestions = "";
             if (questions) {
@@ -96,7 +117,6 @@ const Agent = ({ userName,userId,type,questions }: AgentProps) => {
     };
     const handleDisconnect = async () => {
         setCallStatus(CallStatus.FINISHED);
-
         vapi.stop();
     }
 
